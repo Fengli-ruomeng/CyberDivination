be5ea6f4ad86e5b9460729a3303b29340431690a
Nikolai <15034789+nikolaiF@user.noreply.gitee.com>
2025-02-13 16:03:24 +0800

    重新部署

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,21 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/CyberDivination/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
   optimizeDeps: {
     include: ['openai', 'element-plus']
   },
